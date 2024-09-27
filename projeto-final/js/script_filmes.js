f064121f26f99f1f50e44f05122e484c4e04cb91
Joaogabrielgabry <joaogabrielgabry@gmail.com>
2024-09-27 18:47:50 -0300

    javascript feito home

--- projeto-final/js/script_filmes.js
+++ projeto-final/js/script_filmes.js
@@ -1,36 +1,42 @@
-const modals = document.querySelectorAll('.modal-person');
-const cardButtons = document.querySelectorAll('.person');
-const closeButtons = document.querySelectorAll('.modal__close');
+document.addEventListener('DOMContentLoaded', () => {
+    const modals = document.querySelectorAll('.modal');
+    const cardButtons = document.querySelectorAll('.filme');
+    const closeButtons = document.querySelectorAll('.modal__close');
 
-// Função para ativar o modal
-const openModal = (index) => {
-    modals[index].classList.add('active-modal');
-};
+    // Função para ativar o modal
+    const openModal = (index) => {
+        modals[index].classList.add('active-modal');
+    };
 
-// Função para fechar o modal
-const closeModal = () => {
-    modals.forEach(modal => modal.classList.remove('active-modal'));
-};
+    // Função para fechar todos os modais
+    const closeModal = () => {
+        modals.forEach(modal => modal.classList.remove('active-modal'));
+    };
 
-// Adicionar eventos de clique nos cards
-cardButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        openModal(index);
+    // Adicionar eventos de clique nos filmes para abrir o modal correspondente
+    cardButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            openModal(index);
+        });
     });
-});
 
-// Adicionar evento de clique no botão de fechar
-closeButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        closeModal();
-        event.stopPropagation();
+    // Adicionar eventos de clique nos botões de fechar
+    closeButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            closeModal();
+            event.stopPropagation();
+        });
     });
-});
 
-// Prevenir fechamento ao clicar no modal card
-modals.forEach(modal => {
-    const modalCard = modal.querySelector('.modal_card');
-    modalCard.addEventListener('click', (e) => {
-        e.stopPropagation(); // Impede que o clique no modal card feche o modal
+    // Fechar o modal ao clicar fora do modal_card
+    modals.forEach((modal) => {
+        modal.addEventListener('click', () => {
+            closeModal();
+        });
+
+        const modalCard = modal.querySelector('.modal_card');
+        modalCard.addEventListener('click', (e) => {
+            e.stopPropagation(); // Impede que o clique no modal_card feche o modal
+        });
     });
 });
