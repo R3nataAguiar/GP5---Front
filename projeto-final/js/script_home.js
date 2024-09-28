97e9ea0e48ad832c2d80d5adc01910bdd2f6df09
Joaogabrielgabry <joaogabrielgabry@gmail.com>
2024-09-27 21:19:31 -0300

    ajustes main

--- projeto-final/js/script_home.js
+++ projeto-final/js/script_home.js
@@ -4,12 +4,4 @@ window.addEventListener('scroll', function() {
 
     let opacity = Math.max(1 - scrollPosition / 500, 0.5);
     header.style.backgroundColor = `rgba(26, 61, 124, ${opacity})`;
-
-
-const hamburger = document.getElementById('hamburger');
-const topNav = document.querySelector('.top-nav');
-
-hamburger.addEventListener('click', () => {
-    topNav.classList.toggle('active');
-});
 });
