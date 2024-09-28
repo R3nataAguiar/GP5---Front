9ff9704288a297f8d4a0e3f531c003e84bae07af
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-09-27 23:00:42 -0300

    ajuste barra menu e rolagem da tela

--- projeto-final/js/script_home.js
+++ projeto-final/js/script_home.js
@@ -2,10 +2,6 @@ window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     const scrollPosition = window.scrollY;
 
-    let opacity = Math.max(1 - scrollPosition / 500, 0.5);
-    header.style.backgroundColor = `rgba(26, 61, 124, ${opacity})`;
-
-
 const hamburger = document.getElementById('hamburger');
 const topNav = document.querySelector('.top-nav');
 
