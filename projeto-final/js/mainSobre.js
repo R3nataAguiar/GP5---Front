2c0428f9e77a3625725c20097be89fa2623d1dbf
Joaogabrielgabry <joaogabrielgabry@gmail.com>
2024-10-01 16:24:38 -0300

    ajustes: css, html, script do sobre

--- projeto-final/js/mainSobre.js
+++ projeto-final/js/mainSobre.js
@@ -1,8 +1,6 @@
-/*num mexe pq ta funfano*/
 const modals = document.querySelectorAll('.modal-person');
 const cardButtons = document.querySelectorAll('.person');
 const closeButtons = document.querySelectorAll('.modal__close');
-
 const openModal = (index) => {
     modals[index].classList.add('active-modal');
 };
@@ -10,13 +8,11 @@ const openModal = (index) => {
 const closeModal = () => {
     modals.forEach(modal => modal.classList.remove('active-modal'));
 };
-
 cardButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
         openModal(index);
     });
 });
-
 closeButtons.forEach(button => {
     button.addEventListener('click', (event) => {
         closeModal();
@@ -29,4 +25,24 @@ modals.forEach(modal => {
     modalCard.addEventListener('click', (e) => {
         e.stopPropagation();
     });
+});
+modals.forEach(modal => {
+    modal.addEventListener('click', () => {
+        closeModal();
+    });
+});
+
+document.getElementById('submitButton').addEventListener('click', (event) => {
+    event.preventDefault();
+    const nameInput = document.querySelector('input[type="text"]');
+    const emailInput = document.querySelector('input[type="email"]');
+    const name = nameInput.value;
+    const email = emailInput.value;
+    if (name && email) {
+        alert(`Obrigado ${name}, seu email ${email} foi cadastrado com sucesso!`);
+        nameInput.value = '';
+        emailInput.value = '';
+    } else {
+        alert('Por favor, preencha ambos os campos antes de enviar.');
+    }
 });
